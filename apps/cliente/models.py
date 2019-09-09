ed811d0585821894e0da9d35be852066c7f7c006
hernan_vieira <hernanvieira1997@gmail.com>
2019-09-09 08:49:54 -0300

    Se creo la aplicacion "Cliente" y sus clases

--- apps/cliente/models.py
+++ apps/cliente/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+#Se crean las clases de acuerdo con el diagrama de clases
+class Cliente(models.Model):
+    dni = models.CharField(max_length=10, blank=False, null=False, primary_key=True)
+    apellido = models.CharField(max_length=100, blank=False, null=False)
+    nombre = models.CharField(max_length=100, blank=False, null=False)
+    telefono = models.IntegerField(blank=False, null=False)
+    correo = models.CharField(max_length=200, blank=False, null=False)
+    domicilio = models.TextField(blank = True, null = True)
+    activo = models.BooleanField()
