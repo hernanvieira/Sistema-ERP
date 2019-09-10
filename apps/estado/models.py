3f3acf38fd2ebb314f0f430962efaba97819eb64
Hernan Vieira <hernanvieira1997@gmail.com>
2019-09-10 03:11:34 -0300

    Se agregaron las clases a la aplicacion "estado"

--- apps/estado/models.py
+++ apps/estado/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+#Se crean las clases de acuerdo al diagrama de clases
+
+class Estado (models.Model):
+    id_estado = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    tipo = models.CharField(max_length=50)
+
+class Estado_pedido (models.Model):
+    fecha = models.DateField()
+
+class Estado_prenda (models.Model):
+    fecha = models.DateField()
