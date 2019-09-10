3c40db033818fc747f3b86ca140346d91eb7ad23
Hernan Vieira <hernanvieira1997@gmail.com>
2019-09-10 02:57:56 -0300

    Se agregaron las clases a la aplicacion "prenda"

--- apps/prenda/models.py
+++ apps/prenda/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+#Se crean las clases de acuerdo con diagrama de clases
+
+class Prenda (models.Model):
+    id_prenda = models.AutoField(primary_key = True)
+    talle = models.IntegerField()
+    tiempo_prod_prenda = models.IntegerField()
+    activo = models.BooleanField()
+
+class Tipo_prenda (models.Model):
+    id_tipo_prenda = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+
+class componente (models.Model):
+    id_componente = models.AutoField(primary_key=UserAttributeSimilarityValidator)    
