9f344f0cbcbe82a897a910da0fb54c2783580ae2
Hernan Vieira <hernanvieira1997@gmail.com>
2019-09-10 03:35:37 -0300

    Se agregaron las clases a la aplicacion "material"

--- apps/material/models.py
+++ apps/material/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+#Se crean las clases de acuerdo al diagrama de clases
+class Material (models.Model):
+    id_material = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    color = models.CharField(max_length=50)
+
+class Compra (models.Model):
+    id_compra = models.AutoField(primary_key=True)
+    fecha = models.DateField()
+
+class Unidad_medida (models.Model):
+    id_unidad = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+
+class Tipo_material (models.Model):
+    id_material = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    activo = models.BooleanField()
