6f9933b6915a26ea4a8a5dc49a7f887d37619007
Hernan Vieira <hernanvieira1997@gmail.com>
2019-09-23 18:59:37 -0300

    Se desarrolló el CRUD para gestionar pedidos

--- apps/pedido/urls.py
+++ apps/pedido/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import CrearPedido, ListarPedido, EditarPedido, EliminarPedido
 
 urlpatterns = [
-
+            #URL Pedido
+            path ('crear_pedido/',CrearPedido,name='crear_pedido'),
+            path ('listar_pedido/',ListarPedido,name='listar_pedido'),
+            path ('editar_pedido/<int:id_pedido>',EditarPedido,name='editar_pedido'),
+            path ('eliminar_pedido/<int:id_pedido>',EliminarPedido,name='eliminar_pedido')
 ]
--- apps/pedido/views.py
+++ apps/pedido/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from tp_final.forms import PedidoForm, DetalleForm
+from .models import Pedido, Detalle
+from django.core.exceptions import ObjectDoesNotExist
 
-#Crear Pedido
+#Crear un pedido
 def CrearPedido (request):
     if request.method == 'POST':
         pedido_form = PedidoForm(request.POST)
@@ -10,3 +13,30 @@ def CrearPedido (request):
     else:
         pedido_form = PedidoForm()
     return render(request, 'pedido/crear_pedido.html',{'pedido_form':pedido_form})
+#Listar todos los pedidoes
+def ListarPedido (request):
+    pedidos = Pedido.objects.all()
+    return render(request,'pedido/listar_pedido.html',{'pedidos':pedidos})
+#Editar un pedido
+def EditarPedido (request,id_pedido):
+    try:
+        error = None
+        pedido_form=None
+        pedido = Pedido.objects.get(id_pedido=id_pedido)
+        if request.method=='GET':
+            pedido_form=PedidoForm(instance=pedido)
+        else:
+            pedido_form=PedidoForm(request.POST, instance=pedido)
+            if pedido_form.is_valid():
+                pedido_form.save()
+            return redirect('index')
+    except ObjectDoesNotExist as e:
+        error = e
+    return render(request,'pedido/crear_pedido.html',{'pedido_form':pedido_form, 'error':error})
+#Eliminar un pedido
+def EliminarPedido (request,id_pedido):
+    pedido = Pedido.objects.get(id_pedido=id_pedido)
+    if request.method=='POST':
+        pedido.delete()
+        return redirect('pedido:listar_pedido')
+    return render(request,'pedido/eliminar_pedido.html',{'pedido':pedido})
